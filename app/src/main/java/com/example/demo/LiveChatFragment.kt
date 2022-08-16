69433018a6bb10099bffa5879af2c03ee410953a
Kieran <57554723+ksw5@users.noreply.github.com>
2022-08-16 11:23:25 -0400

    switch to using BoldChat class

--- app/src/main/java/com/example/demo/LiveChatFragment.kt
+++ app/src/main/java/com/example/demo/LiveChatFragment.kt
@@ -12,37 +12,30 @@ import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.demo.databinding.FragmentLiveChatBinding
 import com.integration.async.core.LiveChatListener
+import com.integration.bold.BoldChat
+import com.integration.bold.BoldChatListener
+import com.integration.bold.boldchat.core.PostChatData
 import com.integration.bold.boldchat.visitor.api.*
+import com.integration.bold.boldchat.visitor.api.internal.RPCSender
 import com.integration.core.*
-import com.nanorep.nanoengine.model.conversation.statement.*
-import com.nanorep.sdkcore.model.StatementScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.*
+import com.nanorep.nanoengine.model.conversation.SessionInfo
+import com.nanorep.sdkcore.utils.stringFields
+import java.lang.ref.WeakReference
 
 
 //
-class LiveChatFragment : Fragment(), LiveChatListener {
+class LiveChatFragment : Fragment(), BoldChatListener {
 
     private var _binding: FragmentLiveChatBinding? = null
     val binding get() = _binding!!
 
     val key = BuildConfig.LiveAgentKey
-    val account: Account = Account(key)
 
-
-    lateinit var listener: CreateChatListener
-    lateinit var agentResponse : OnStatementResponse
-
+    private val boldChat: BoldChat = BoldChat()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         createChat()
-
-
     }
 
 
@@ -53,12 +46,25 @@ class LiveChatFragment : Fragment(), LiveChatListener {
         // Inflate the layout for this fragment
         _binding = FragmentLiveChatBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.agentResponse.text = "An agent will be with you shortly..."
+
+        binding.chatBox.setEndIconOnClickListener {
+            val userMessage = binding.editText.text
+            boldChat.postMessage(userMessage.toString())
+            binding.userMessage.text = userMessage.toString()
+            it.hideKeyboard()
+            userMessage?.clear()
+        }
+        val id = boldChat.visitorInfo.id
+        val timeStamp = System.currentTimeMillis()/1000
+        val sender = PersonType.Operator
+        val message = sender
 
 
     }
@@ -66,42 +72,49 @@ class LiveChatFragment : Fragment(), LiveChatListener {
 
     private fun createChat() {
 
-        listener = object : CreateChatListener {
-            override fun onChatCreated(p0: PreChat?, p1: Chat?) {
-
-                Handler(Looper.getMainLooper()).post(Runnable {
-                    binding.chatBox.setEndIconOnClickListener {
-                        val userMessage = binding.editText.text
-                        p1?.sendMessage(userMessage.toString())
-                        Log.d("user_message", userMessage.toString())
-                        binding.userMessage.text = userMessage.toString()
-                        it.hideKeyboard()
-                        userMessage?.clear()
-
-                    }
-                })
-
-
-            }
-
-            override fun onChatUnavailable(
-                p0: UnavailableReason?,
-                p1: UnavailableForm?,
-                p2: Chat?,
-                p3: MutableMap<String, String>?
-            ) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onChatCreateFailed(p0: Int, p1: String?) {
-                TODO("Not yet implemented")
-            }
-
-
+        val info = SessionInfo("").apply {
+            addConfigurations("skipPreChat" to true)
         }
 
-        account.createChat(listener)
+        val boldChatListener = object : BoldChatListener {
+            override fun error(code: Int, message: String?, data: Any?) {
+                // implement to handle errors
+            }
 
+            override fun chatAccepted(timestamp: Long, operator: Chatter) {
+                // at this point the agent accepted user's chat, the chat won't be canceled due to
+                // acceptance timeout, and the chat can start.
+            }
+
+            override fun messageArrived(
+                messageId: String,
+                message: String,
+                timestamp: Long,
+                sender: String
+            ) {
+                // agent messages will be received on this implementation
+                // sender should indicate one of PersonType enum. agent messages are followed with sender-> Operator
+
+            }
+
+            override fun chatEnded(formData: PostChatData?) {
+                // Chat ended.
+
+            }
+
+            override fun visitorInfoUpdated() {
+                // visitorInfo property was updated on the Boldchat with ChatId and visitorId
+                // in case needed for the app. (visitorId is used for returning chats of the same user)
+            }
+        }
+
+        boldChat.wListener = WeakReference(boldChatListener)
+
+        boldChat.visitorInfo = info
+
+        boldChat.prepare(requireContext(), key, HashMap())
+
+        boldChat.start()
 
     }
 
@@ -111,6 +124,5 @@ class LiveChatFragment : Fragment(), LiveChatListener {
             context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
-
 
 }
